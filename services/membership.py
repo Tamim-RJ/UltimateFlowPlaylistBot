d0aed4aa68e29eaaa8bacd61a32c7516e41f2198
Tamim-RJ <tamimrj90@gmail.com>
2026-07-09 21:03:28 +0330

    Fixed Telegram member not found error

--- services/membership.py
+++ services/membership.py
@@ -1,4 +1,5 @@
 from aiogram import Bot
+from aiogram.exceptions import TelegramBadRequest
 from aiogram.types import User
 
 from config import CHANNEL_USERNAME
@@ -8,5 +9,10 @@ async def check_membership(bot: Bot, user: User):
     if not CHANNEL_USERNAME:
         return True
 
-    member = await bot.get_chat_member(chat_id=CHANNEL_USERNAME, user_id=user.id)
-    return member.status in ["member", "administrator", "creator"]
+    try:
+        member = await bot.get_chat_member(chat_id=CHANNEL_USERNAME, user_id=user.id)
+        return member.status in ["member", "administrator", "creator"]
+    except TelegramBadRequest as exc:
+        if "member not found" in str(exc).lower():
+            return False
+        raise
